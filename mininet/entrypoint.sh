dd63e48d79d80327fdc54889cc3051cadf945a0a
rlia <ryosuke.sakurai.4w@stu.hosei.ac.jp>
2019-07-07 11:57:31 +0900

    bridge crached

--- mininet/entrypoint.sh
+++ mininet/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env /bin/bash
 
-
 service openvswitch-switch start
 ovs-vsctl set-manager ptcp:6640
-mn --controller=remote,ip=127.0.0.1,port=6653
+mn --controller=remote,ip=172.20.0.2 --switch=ovsk,protocols=OpenFlow13
 
 #tail -f /dev/null
 bash
