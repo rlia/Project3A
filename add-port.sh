ac1761202fa01aac92640ccac71b1e1c2d3a2102
Rlia <34536530+rlia@users.noreply.github.com>
2019-08-03 15:31:00 +0900

    Update add-port.sh

--- add-port.sh
+++ add-port.sh
@@ -36,9 +36,9 @@ ovs-vsctl add-port ovs-br4 patch4-2 -- set interface patch4-2 type=patch options
 ovs-vsctl add-port ovs-br4 patch4-3 -- set interface patch4-3 type=patch options:peer=patch3-4
 
 #ovs-docker add-port ovs-br1 eth1 mininet --ipaddress=172.21.0.3/24
-ovs-docker add-port ovs-br1 eth1 open_day_light --ipaddress=172.21.0.2/24
-ovs-docker add-port ovs-br1 eth1 rtmp_server --ipaddress=172.21.0.4/24
-ovs-docker add-port ovs-br1 eth1 stream_server --ipaddress=172.21.0.5/24
+ovs-docker add-port ovs-br4 eth1 open_day_light --ipaddress=172.21.0.2/24
+ovs-docker add-port ovs-br4 eth1 rtmp_server --ipaddress=172.21.0.4/24
+ovs-docker add-port ovs-br4 eth1 stream_server --ipaddress=172.21.0.5/24
 #ovs-vsctl add-port ovs-br1 ens37
 #ovs-vsctl set bridge ovs-br1 protocols=OpenFlow13
 #ovs-vsctl set-controller ovs-br1 tcp:172.21.0.2
