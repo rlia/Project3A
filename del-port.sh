9c7c00f8bb62485c184926904ceea5c8f40bbdc4
Rlia <34536530+rlia@users.noreply.github.com>
2019-08-03 15:31:53 +0900

    Update del-port.sh
    update  > ovs-br4

--- del-port.sh
+++ del-port.sh
@@ -2,9 +2,9 @@
 
 
 #ovs-docker del-port ovs-br1 eth1 mininet
-ovs-docker del-port ovs-br1 eth1 open_day_light
-ovs-docker del-port ovs-br1 eth1 rtmp_server
-ovs-docker del-port ovs-br1 eth1 stream_server
+ovs-docker del-port ovs-br4 eth1 open_day_light
+ovs-docker del-port ovs-br4 eth1 rtmp_server
+ovs-docker del-port ovs-br4 eth1 stream_server
 
 
-#ovs-vsctl clear Bridge ovs-br1 ports
+#ovs-vsctl clear Bridge ovs-br4 ports
