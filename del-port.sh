2c153cc2e3dbffadc208a6e560736c39e514e4e1
Rlia <34536530+rlia@users.noreply.github.com>
2019-08-03 10:46:53 +0900

    add command

--- del-port.sh
+++ del-port.sh
@@ -5,3 +5,6 @@
 ovs-docker del-port ovs-br1 eth1 open_day_light
 ovs-docker del-port ovs-br1 eth1 rtmp_server
 ovs-docker del-port ovs-br1 eth1 stream_server
+
+
+#ovs-vsctl clear Bridge ovs-br1 ports
